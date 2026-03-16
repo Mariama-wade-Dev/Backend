9d058e40a59630248eef9652b24766c1c782e66d
Mariama <wadeseydamariama@gmail.com>
2026-03-16 00:31:11 +0000

    Fix: URL forgot-password et reset-password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 from .views import RegisterView, MyTokenObtainPairView # Importe ta nouvelle vue ici
 from rest_framework_simplejwt.views import TokenRefreshView
+from django_rest_passwordreset.views import ResetPasswordConfirm # Ajoute cet import
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='auth_register'),
-    # On remplace TokenObtainPairView par MyTokenObtainPairView
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # Remplace la ligne password_reset par celle-ci :
+    
+    # Pour le bouton "Mot de passe oublié" (Demander le lien)
     path('forgot-password/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    
+    # Pour le formulaire "Réinitialiser" (Changer le mot de passe)
+    path('reset-password/', include('django_rest_passwordreset.urls', namespace='password_reset_confirm')),
 ]
