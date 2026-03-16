4a066029e7d86854a7080e74e51ddf76f0ffa0b9
Mariama <wadeseydamariama@gmail.com>
2026-03-16 11:33:09 +0000

    ajout modification

--- core/settings.py
+++ core/settings.py
@@ -89,7 +89,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
